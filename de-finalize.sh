54f322c8a424afb7c626b08a552484a2750af832
Giles Knap <giles.knap@diamond.ac.uk>
2024-07-03 15:24:47 +0100

    remove redundant files

--- de-finalize.sh
+++ de-finalize.sh
@@ -1,3 +1,5 @@
+# a get out of jail script for when you have made some dodgy argo cd apps
+# that get stuck deleting
 for app in $(kubectl get apps -o name); do
-    kubectl patch $app -n hgv27681  -p '{"metadata":{"finalizers":null}}' --type=merge
+    kubectl patch $app -p '{"metadata":{"finalizers":null}}' --type=merge
 done
